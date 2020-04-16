a323e5ff3079653399b1054aa34fa734bb9ddac7
Zhunisali <zshanabek@gmail.com>
2020-04-17 01:14:18 +0600

    refactor

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,39 +1,30 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
-const ShortUrls = require("../models/ShortUrls");
-// get base URL
+
+const ShortUrls = require("../models/ShortUrls")
 const baseUrl = process.env.BASE_URL;
-/**
- * @route POST api/short
- * @desc Short a given URL
- * @access public
- */
+
 router.post("/short", (req, res, next) => {
     let shortUrl;
+
     if (isEmpty(req.body)) {
-        res
-            .status(400)
-            .json({ success: false, msg: "Data missing!", data: req.body });
+        res.status(400).json({
+            success: false, msg: "Data missing!", data: req.body
+        });
     }
-    // Create a shorturl object
     shortUrl = new ShortUrls({
         url: req.body.url,
     });
     createAndSaveShortUrl(shortUrl, res);
 });
-/**
- * @route GET api/urlID
- * @desc Redirect to actual url based on URL ID
- * @access public
- */
+
 router.get("/:id", (req, res, next) => {
     const urlCode = req.params.id;
-    console.log('url id', urlCode);
 
+    console.log('url id', urlCode);
     ShortUrls.findOne({ urlCode: urlCode }, (err, urlObj) => {
         if (err) {
-            console.log(err);
             res.status(500).json({ success: false, msg: "Internal Server Error!" });
             return;
         }
@@ -52,8 +43,6 @@ router.get("/:id", (req, res, next) => {
                     return;
                 }
                 console.log(urlObj);
-                console.log("mol===")
-                console.log(redirectTo)
                 // Redirect to actual URL
                 res.redirect(301, redirectTo);
             }
@@ -62,25 +51,26 @@ router.get("/:id", (req, res, next) => {
 });
 
 function createAndSaveShortUrl(shortUlrObj, res) {
-    // Generate a random string to replace the url
+    // random string to replace the url
     let randomStr = generateRandomString();
     // Check if the random string already exist in DB
     ShortUrls.findOne({ urlCode: randomStr }, (err, url) => {
-        if (err)
-            console.log(err);
-        else if (url == null || isEmpty(url)) {
+        if (url == null || isEmpty(url)) {
             console.log("url obj", url, randomStr);
             shortUlrObj.urlCode = randomStr;
             // Not a duplicate
             shortUlrObj.save(err => {
                 if (err) {
-                    res.status(400).json({ success: true, msg: err });
+                    res.status(400).json({
+                        success: true, msg: err
+                    });
                 }
-                res.status(200).json({ success: true, url: baseUrl + randomStr });
+                res.status(200).json({
+                    success: true, url: baseUrl + randomStr
+                });
             });
-        } else {
+        } else
             saveShortUrl(shortUlrObj);
-        }
     });
 }
 
--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const apiController = require('./controllers/apiController')
 // To access our .env file values
 require("dotenv").config();
 
+const app = express();
+
 // Set up port
 const port = process.env.PORT || 5000;
-const app = express();
+const dbURL = process.env.MONGODB_URL;
 
 // Set BodyParser middleware
 app.use(bodyParser.json());
-// Get the DB conection string
-const dbURL = process.env.MONGODB_URL;
-// Connect to the database
+
 
 mongoose
   .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,13 +23,10 @@ mongoose
     err => console.error(err)
   );
 
-// For testing purpose only
 app.get('/hello', function (req, res, next) {
   return res.json({ msg: 'Hello world' });
 })
 
-// Redirct any route which includes /api to apiRoute controller
 app.use("/api", apiController);
 
-// Start listening
 app.listen(port, () => console.log("Server started at port " + port));
